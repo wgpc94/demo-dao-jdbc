0199f06258754ee049a43eba01ecb4b452d0a6fe
welington gomes <welingtonsc94@gmail.com>
2020-09-16 10:57:14 -0300

    Criando Seller

--- src/aplication/Progam.java
+++ src/aplication/Progam.java
@@ -1,13 +1,19 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Progam {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "Bob@email.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
