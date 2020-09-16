e4ac174a889936a18dab5e97019ed1f820330afc
welington gomes <welingtonsc94@gmail.com>
2020-09-16 10:26:34 -0300

    criando Department

---
+++ src/aplication/Progam.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Progam {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
